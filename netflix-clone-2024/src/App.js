beb0b63409ebdf9664667181d162facd22141056
Yeshihareg Tadesse <tadehareg@gmail.com>
2024-07-03 16:22:39 +0300

    header and footer

--- netflix-clone-2024/src/App.js
+++ netflix-clone-2024/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hellow</h1>
-          </div>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   );
 }
 
