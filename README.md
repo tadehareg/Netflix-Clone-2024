e00adc3aef2c728fee1563bb01224d3e55e54db4
Yeshihareg Tadesse <tadehareg@gmail.com>
2024-07-04 15:24:50 +0300

    axios and request

--- README.md
+++
@@ -1,1 +0,0 @@
-# Netflix-Clone-2024
---
+++ netflix-clone-2024/src/utils/requests.js
@@ -0,0 +1,12 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const request = {
+     fetchTrending:`/trending/all/week?api_key=${API_KEY}&llanguage=en-US,`,
+     fetchNetflixOrginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+     fetchTopRatedMovies:`/discover/movie?api_key=${API_KEY}c&with_genres=35`,
+     fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+     fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+     fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+     fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=49`,
+     fetchTvShow:`/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+};  
+export default request;   
